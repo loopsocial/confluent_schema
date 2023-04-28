a5d866736df7eccd0e02275f005b6aa1d01f4d15
Alexandre Costa <alexandremcost@gmail.com>
2023-04-28 18:12:12 -0300

    prepare to publish on hex

--- mix.exs
+++ mix.exs
@@ -5,10 +5,21 @@ defmodule ConfluentSchema.MixProject do
     [
       app: :confluent_schema,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      description: "Confluent Schema Cache and Validation",
+      elixir: "~> 1.12",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
+    ]
+  end
+
+  def package() do
+    [
+      oroganization: "loopsocial",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
   end
 
@@ -28,7 +39,8 @@ defmodule ConfluentSchema.MixProject do
       {:confluent_schema_registry, "~> 0.1.1"},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
       {:ex_json_schema, "~> 0.9.2"},
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
