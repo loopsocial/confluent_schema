8642658c4a8d041643ed01716a8fc120896c5d5b
Alexandre Costa <alexandremcost@gmail.com>
2023-04-28 16:40:03 -0300

    write doctests for ConfluentSchema module and fix types

--- lib/confluent_schema.ex
+++ lib/confluent_schema.ex
@@ -3,19 +3,29 @@ defmodule ConfluentSchema do
   Provides cache and validation for confluent schemas.
   """
   alias ConfluentSchema.Cache
+  alias ExJsonSchema.Validator
+
+  @type errors :: [{message :: binary, path :: binary}]
 
   @doc """
   Validates the payload against the schema for the given subject.
 
-  Return `{:ok, payload}` if the subject is found on cache and payload is valid.
-  Return `{:error, :not_found}` if the subject is not found.
-  Return `{:error, ExJsonSchema.errors()}` if the payload is invalid.
+  ## Examples
+
+      iex> ConfluentSchema.validate("a string example", "subject")
+      {:ok, "a string example"}
+
+      iex> ConfluentSchema.validate("a string example", "unknown-subject")
+      {:error, :not_found}
+
+      iex> ConfluentSchema.validate(123, "subject")
+      {:error, [{"Type mismatch. Expected String but got Integer.", "#"}]}
   """
   @spec validate(map, binary) ::
-          {:ok, map} | {:error, :not_found} | {:error, ExJsonSchema.errors()} | no_return
+          {:ok, map} | {:error, :not_found} | {:error, errors} | no_return
   def validate(payload, subject) do
     with {:ok, schema} <- Cache.get(subject),
-         :ok <- ExJsonSchema.Validator.validate(schema, payload) do
+         :ok <- Validator.validate(schema, payload) do
       {:ok, payload}
     end
   end
--- test/confluent_schema_test.exs
+++ test/confluent_schema_test.exs
@@ -1,4 +1,11 @@
 defmodule ConfluentSchemaTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  alias ConfluentSchema.Server
   doctest ConfluentSchema
+
+  setup do
+    RegistryMock.set_global_subject("subject")
+    start_supervised!({Server, [adapter: Tesla.Mock]})
+    Server.wait_start()
+  end
 end
